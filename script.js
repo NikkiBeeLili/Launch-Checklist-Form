c7f8b0a5eb341b9f599a8f5453418eec7b2f3a50
NikkiBeeLili <41345938+NikkiBeeLili@users.noreply.github.com>
2020-11-03 14:35:59 -0600

    Add files via upload
    -fixed faultyItems and conditionals

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Write your JavaScript code here!
+//The validation for being blank works on all fields, as does the validation for invalid types of inputs. 
+//The bottom of the page looks good, with the right information when the form is invalid. 
+//The only issue is that the page refreshes when you click submit and all of the fields are valid. 
+ 
+//Other than that, you could also combine some of your conditionals to simultaneously check cargo weight AND fuel level,
+// which would make your code more concise.
 
-//not sure where else to add preventDefault()
 window.addEventListener("load", function(response) {
    let formSubmit = document.getElementById('formSubmit');  
    formSubmit.addEventListener("click", function(event) { 
+      event.preventDefault(); 
       let pilot = document.getElementById('pilotName').value; 
       let coPilot = document.querySelector('input[name=copilotName]').value; 
       let fuelLevel = document.querySelector('input[name=fuelLevel]').value; 
@@ -16,8 +22,7 @@ window.addEventListener("load", function(response) {
       let faultyItems= document.getElementById('faultyItems')
       
 if (pilot === ''|| coPilot === '' ||fuelLevel === ''||cargoMass ==='') {
-   alert("All fields are required!");
-   launchStatus.innerHTML = "Shuttle is not ready for launch.";
+    alert("All fields are required!");
    event.preventDefault(); 
    
 
@@ -25,41 +30,36 @@ if (pilot === ''|| coPilot === '' ||fuelLevel === ''||cargoMass ==='') {
 
 else if(!isNaN(pilot) || !isNaN(coPilot)  || isNaN(fuelLevel) ||  isNaN(cargoMass)){
    alert("Make sure to enter valid information for each field!"); 
-   launchStatus.innerHTML = "Shuttle is not ready for launch.";
    event.preventDefault(); 
   
 } 
-//else {
-   //pilotStatus.innerHTML = `Pilot: ${pilot} Ready.`; 
-   //coPilotStatus.innerHTML = `Co-Pilot: ${coPilot} Ready.`; 
-//}
+
  
   
 
 
-   if (fuelLevel < 10000){
+   else if (fuelLevel < 10000 || cargoMass > 10000){
+      event.preventDefault();
       faultyItems.style.visibility = "visible"; 
       pilotStatus.innerHTML = `Pilot ${pilot} is Ready.`; 
       coPilotStatus.innerHTML = `Co-Pilot ${coPilot} is Ready.`; 
       launchStatus.innerHTML = "Shuttle is not ready for launch.";
-      fuelStatus.innerHTML = "There is not enough fuel for the journey!";
       launchStatus.style.color = "red"; 
-      event.preventDefault(); 
-  } 
-  else if (cargoMass > 10000){
-   faultyItems.style.visibilty = "visible"; 
-   pilotStatus.innerHTML = `Pilot ${pilot} is Ready.`; 
-   coPilotStatus.innerHTML = `Co-Pilot ${coPilot} is Ready.`; 
-   launchStatus.innerHTML = "Shuttle is not ready for launch.";
-   cargoStatus.innerHTML = "There is too much mass for the shuttle to take off.";
-   launchStatus.style.color = "red";
-   event.preventDefault(); 
+         (fuelLevel < 10000)
+         fuelStatus.innerHTML = "There is not enough fuel for the journey!";
+         event.preventDefault(); 
+      
+         (cargoMass > 10000)
+         cargoStatus.innerHTML = "There is too much mass for the shuttle to take off."
+         event.preventDefault(); 
 
+   
 
   }
 
  else {
  launchStatus.innerHTML = "Shuttle is ready for launch"; 
+ faultyItems.style.visibility = "visible";
  pilotStatus.innerHTML = `Pilot ${pilot} is Ready.`; 
 coPilotStatus.innerHTML = `Co-Pilot ${coPilot} is Ready.`; 
  launchStatus.style.color = "green"; 
@@ -93,7 +93,6 @@ fuelStatus.innerHTML = "Cargo mass is light enough for launch.";
         <li>${json[index].moons}</li>
        </ol>
         <img src= ${json[index].image} height=250></img> 
-
 `;
 
       
@@ -104,32 +103,3 @@ fuelStatus.innerHTML = "Cargo mass is light enough for launch.";
 });
 
  }); 
-
-     
-
- //Change the lists to reflect objects and values(too?)     
-   
-
-
-
-
-// more event.preventDefaults
-//I need the event.preventDefault probably for the missionTarget section. 
-
-
-
-
-
-
-//document.getElementById (replace HTML with Javascript, innerHTML)
-
-   /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}"> */ 
